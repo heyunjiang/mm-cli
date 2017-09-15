876aff5a4b5826e9e9c7e5308ebafc6fe3e87eff
heyunjiang <18224487974@163.com>
2017-09-15 17:26:22 +0800

    first step

---
+++ mww/bin/mww.js
@@ -0,0 +1,34 @@
+#!/usr/bin/env node
+
+'use strict';
+
+var program = require('commander');
+
+const showVersion = ()=>{
+	console.log(program.version)
+}
+
+program
+  .version('0.0.1')
+  .option('-v, --version', 'show version', showVersion);
+
+program
+    .command('list')
+    .description('list files in current working directory')
+    .option('-a, --all', 'Whether to display hidden files')
+    .action(function(options) {
+        var fs = require('fs');
+        fs.readdir(process.cwd(), function(err, files) {
+            var list = files;
+            if (!options.all) {
+                list = files.filter(function(file) {
+                    return file.indexOf('.') !== 0;
+                });
+            }
+            console.log(list.join(' '));
+        });
+    });
+
+program.parse(process.argv);
+
+console.log('heyunjiang');
